6e1cc73361deffeeca5dc3852a7074eca71ab75d
Álvaro Herero <alvaro.herrero11x@gmail.com>
2025-04-08 20:08:39 +0200

    Configures Tailwind CSS and PrimeNG
    Sets up Tailwind CSS and PrimeNG for styling and UI components.
    
    Adds necessary dependencies and configuration files
    for integrating Tailwind CSS and PrimeNG into the project.
    
    Removes the default Angular template from app.component.html
    and replaced it with a basic tailwind styled component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MessageService} from 'primeng/api';
+
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  standalone: true,
+  styleUrl: './app.component.css',
+  providers: [MessageService],
 })
 export class AppComponent {
   title = 'Entrena-Sync-Web-Client';
