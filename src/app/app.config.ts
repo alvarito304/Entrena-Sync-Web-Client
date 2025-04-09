78cd44641ac809dd376053f1be7e7b4d414971a2
Álvaro Herero <alvaro.herrero11x@gmail.com>
2025-04-09 14:06:43 +0200

    Configures app and adds layout styling
    Sets up initial app configuration, including router configurations for in-memory scrolling and blocking initial navigation.
    Adds basic layout structure and styling.
    Adds a flag component with associated styling.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {provideRouter, withEnabledBlockingInitialNavigation, withInMemoryScrolling} from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
@@ -7,24 +7,24 @@ import {providePrimeNG} from 'primeng/config';
 import  Aura  from '@primeng/themes/aura';
 import {provideAnimations} from '@angular/platform-browser/animations';
 import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
+import {provideHttpClient, withFetch} from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
-    provideAnimations(),
+    provideRouter(routes, withInMemoryScrolling({ anchorScrolling: 'enabled', scrollPositionRestoration: 'enabled' }), withEnabledBlockingInitialNavigation()),
+    provideHttpClient(withFetch()),
     provideAnimationsAsync(),
     provideClientHydration(withEventReplay()),
-    providePrimeNG({
-      theme: {
-        preset: Aura,
-        options: {
+    providePrimeNG({ theme: {
+      preset: Aura, options: {
+        darkModeSelector: '.app-dark',
           cssLayer: {
             name: 'primeng',
             order: 'theme, base, primeng'
           }
-        }
       }
+    }
     }),
     provideAnimations()
   ]
