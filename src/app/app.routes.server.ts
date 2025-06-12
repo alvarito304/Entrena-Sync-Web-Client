424c14a3323f34bbf2e7844bf6066abf9f720f2e
Javier Hernández vicente <javier.hernandez@alumno.iesluisvives.org>
2025-06-12 14:08:09 +0200

    FIX: on routes server for deploy

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -6,6 +6,10 @@ export const serverRoutes: ServerRoute[] = [
     renderMode: RenderMode.Server
   },
   {
+    path: 'trainers',
+    renderMode: RenderMode.Server
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
